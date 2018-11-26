d5e834a089c76d8ee598f99deb39a88d7ffe6d78
Joy <stepjoyous@gmail.com>
2018-11-26 15:53:12 -0800

    Completed Part 1: The Gladiator... created Gladiator constructor function

---
+++ gladiator.js
@@ -0,0 +1,8 @@
+function Gladiator(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+};
+
+var max = new Gladiator("Maxiums", "Trident");
+console.log(max.name);
+console.log(max.weapon);
