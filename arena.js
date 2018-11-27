0aa8c3442f6ba61f5a4e12cabf3d0b6041e1c4fe
Joy <stepjoyous@gmail.com>
2018-11-26 16:44:01 -0800

    Simplified and finalized capitalization of Arena names

--- arena.js
+++ arena.js
@@ -1,13 +1,11 @@
 // ----------PART II: The Arena ---------------
 function Arena(name) {
-    this.name = name;
-    // ---- Function to Capitalize Name ---
-    const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
-    console.log(nameCapitalized);   
+    this.name = name.charAt(0).toUpperCase() + name.slice(1);
+    return(this.name);   
 };
 
 var colosseum = new Arena("Colosseum");
 console.log(colosseum.name);
 
-var colosseum = new Arena("megalopolis");
+var colosseum = new Arena("megalopolis")
 console.log(colosseum.name);
